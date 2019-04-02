177094d77392aec105a0e759ec2f2f1999720800
Emily <cookie0915n@gmail.com>
2019-04-02 19:25:12 +0000

    Add user login test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "Chapter 3: Mostly static pages"
-  end
+  include SessionsHelper
 end
