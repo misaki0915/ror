72704761485d528d42a8f3915e72d800295ef4c2
Emily <cookie0915n@gmail.com>
2019-04-10 17:14:30 +0000

    remember user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,32 @@
-class SessionsController < ApplicationController
+module SessionsHelper
 
-  def new
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 現在ログインしているユーザーを返す (いる場合)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
-  def destroy
-    log_out
-    redirect_to root_url
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
