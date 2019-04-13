14f9d81526e015daab549f6f8d9df5735bcdf98e
Emily <cookie0915n@gmail.com>
2019-04-13 17:21:03 +0000

    Fix two bags

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,22 @@
-module SessionsHelper
+class SessionsController < ApplicationController
 
-  # 渡されたユーザーでログインする
-  def log_in(user)
-    session[:user_id] = user.id
+  def new
   end
 
-  # ユーザーのセッションを永続的にする
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  # 現在ログインしているユーザーを返す (いる場合)
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
-  def logged_in?
-    !current_user.nil?
-  end
-
-  # 現在のユーザーをログアウトする
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
